adabf1785a854340e303b71acb259319b910adb3
matheusleo <matheusleotrab@gmail.com>
2022-07-12 21:12:35 -0300

    modify how the styling from the SimpleButton is setted

--- components/SimpleButton/SimpleButton.jsx
+++ components/SimpleButton/SimpleButton.jsx
@@ -1,8 +1,18 @@
-import styles from './SimpleButton.module.css'
 import { Button } from '@mui/material';
 
 const SimpleButton = ({ onClick, children, passStyle }) => (
-  <Button type="submit" onClick={onClick} style={passStyle} className={styles.button}>
+  <Button type="submit" onClick={onClick} style={passStyle} sx={{
+    'height': '3em',
+    'background-color': '#D1151A',
+    'font-size': '1em',
+    'width': '8em',
+    'color': '#FFFFFF',
+    'border': 'none',
+    'border-radius': '5px',
+    'align-self': 'center',
+    'cursor': 'pointer',
+    'transition': 'all 0.3s cubic-bezier(0.42, 0, 0.58, 1)',
+  }}>
     {children}
   </Button>
 )
