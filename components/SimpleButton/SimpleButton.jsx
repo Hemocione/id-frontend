611917c931510801f49bfd7b033d1a82345898df
Guilherme Bergman de Souza <bergmanguilherme2k@gmail.com>
2022-09-13 21:53:32 -0300

    fix 'entrar' button

--- components/SimpleButton/SimpleButton.jsx
+++ components/SimpleButton/SimpleButton.jsx
@@ -1,18 +1,8 @@
-import { Button } from '@mui/material';
+import { Button } from '@mui/material'
+import styles from './SimpleButton.module.css'
 
 const SimpleButton = ({ onClick, children, passStyle }) => (
-  <Button type="submit" onClick={onClick} style={passStyle} sx={{
-    'height': '3em',
-    'background-color': '#D1151A',
-    'font-size': '1em',
-    'width': '8em',
-    'color': '#FFFFFF',
-    'border': 'none',
-    'border-radius': '5px',
-    'align-self': 'center',
-    'cursor': 'pointer',
-    'transition': 'all 0.3s cubic-bezier(0.42, 0, 0.58, 1)',
-  }}>
+  <Button type="submit" onClick={onClick} style={passStyle} className={styles.button}>
     {children}
   </Button>
 )
