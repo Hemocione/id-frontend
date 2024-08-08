5914f7ee90b3cbb6f1e93b0f672d9aad6d0eb6ca
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-08-07 22:09:44 -0300

    attempt cookie fix

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,32 +1,32 @@
-function setCookie(cname, cvalue, exdays, domain) {
+function setCookie(cname, cvalue, exdays, domain = "hemocione.com.br") {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  let _domain = "domain=" + domain;
-  document.cookie =
-    cname + "=" + cvalue + ";" + expires + ";" + _domain + ";" + ";path=/";
+  const expires = "expires=" + d.toUTCString();
+  const _domain = domain ? "domain=" + domain + ";" : "";
+  document.cookie = `${cname}=${cvalue};${expires};${_domain}path=/`;
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == " ") {
+    while (c.charAt(0) === " ") {
       c = c.substring(1);
     }
-    if (c.indexOf(name) == 0) {
+    if (c.indexOf(name) === 0) {
       return c.substring(name.length, c.length);
     }
   }
   return undefined;
 }
 
-function deleteCookie(name) {
+function deleteCookie(name, domain = "hemocione.com.br") {
   if (!getCookie(name)) return;
   // delete cookie by setting the expiration date to a past date
-  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  const _domain = domain ? "domain=" + domain + ";" : "";
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; ${_domain}path=/`;
 }
 
 export { setCookie, getCookie, deleteCookie };
